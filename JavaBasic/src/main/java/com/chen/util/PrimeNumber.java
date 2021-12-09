a499fabf67c411eb9d6a55d79d82ebf66a842f97
陈棋烽 <chen@Iriss-Mac.local>
2021-12-10 01:34:53 +0800

    modify compiler version

--- JavaBasic/src/main/java/com/chen/util/PrimeNumber.java
+++ JavaBasic/src/main/java/com/chen/util/PrimeNumber.java
@@ -1,2 +1,52 @@
-package com.chen.util;public class PrimeNumber {
+package com.chen.util;
+
+import org.junit.Test;
+
+public class PrimeNumber {
+    /**
+     * 打印一定范围以内质数
+     */
+    @Test
+    public void PrimeNumberPrint(){
+        int i,j;
+        for(i=1;i<=1000;i++){
+            if(i==1 || i==2){
+                System.out.print(i+",");
+                continue;
+            }
+            for(j=2;j<i;j++){
+                if(i%j==0)
+                    break;
+                if(j==i-1)
+                    System.out.print(i+",");
+            }
+        }
+    }
+
+    /**
+     * 打印质数塔
+     */
+    @Test
+    public void PrimeNumberTower(){
+        int h = 1;//代表行号
+        int num = 0;//代表每行中质数个数
+        for(int i=1;i<=1000;i++){
+            int count = 0;//当前元素能被整除的个数
+            for (int x = 1; x <= i; x++) {
+                if (i % x == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                System.out.print(i + "\t");
+                num++;
+
+                if (h == num) {
+                    System.out.println();
+                    h++;
+                    num = 0;
+                }
+            }
+        }
+    }
 }
