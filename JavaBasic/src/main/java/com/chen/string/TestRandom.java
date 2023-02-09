4b4bfcce60a4488dc7a04696a5f1149d91f4ce27
Qifeng Chen <chenqifengfree@gmail.com>
2023-02-09 13:39:32 +0800

    add TestRandom

---
+++ JavaBasic/src/main/java/com/chen/string/TestRandom.java
@@ -0,0 +1,37 @@
+package com.chen.string;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TestRandom {
+
+    @Test
+    public void testMathRandom(){
+        int max = 200;
+        int min = 100;
+        double random = Math.random()*(max - min);
+        System.out.println(random);
+    }
+
+    @Test
+    public void testThreadLocalRandom(){
+        //int max = 200;
+        //int min = 100;
+        ThreadLocalRandom tlr = ThreadLocalRandom.current();
+        int random = tlr.nextInt(100,200);
+        System.out.println(random);
+    }
+
+    @Test
+    public void randomID(){
+        ThreadLocalRandom tlr = ThreadLocalRandom.current();
+        int random = tlr.nextInt(1000,8000);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        String dateStr = sdf.format(new Date());
+        String nextId = "tag_test_" + dateStr + String.format("%d", random);
+        System.out.println(nextId);
+    }
+}
